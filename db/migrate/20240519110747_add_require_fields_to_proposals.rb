7b3faf9673e4cd9ee72c96aa22d3ba6a99e11b63
barbara oliveira <barbarafictiovore@gmail.com>
2024-05-19 13:26:56 +0200

    lint

--- db/migrate/20240519110747_add_require_fields_to_proposals.rb
+++ db/migrate/20240519110747_add_require_fields_to_proposals.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddRequireFieldsToProposals < ActiveRecord::Migration[6.1]
   def change
     add_column :decidim_proposals_proposals, :require_category, :boolean
