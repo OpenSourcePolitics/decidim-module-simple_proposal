3513bf9dbe0b210cf79178939c2383585f6c9eb4
barbara oliveira <barbarafictiovore@gmail.com>
2024-07-25 14:18:50 +0200

    update rspec

--- spec/helpers/decidim/scopes_helper_spec.rb
+++ spec/helpers/decidim/scopes_helper_spec.rb
@@ -28,5 +28,21 @@ module Decidim
         expect(actual).to have_equivalent_markup_to(expected)
       end
     end
+    describe "#ancestors" do
+        let(:organization_1) { create(:organization) }
+        let(:organization_2) { create(:organization) }
+
+        let(:scope_1_o_1) { create(:scope, organization: organization_1)}
+        let(:scope_2_o_1) { create(:scope, organization: organization_1)}
+        let(:scope_1_o_2) { create(:scope, organization: organization_2)}
+        let(:scope_2_o_2) { create(:scope, organization: organization_2)}
+
+        it "returns only the scopes for the current organization" do
+          allow(helper).to receive(:current_organization).and_return(organization_1)
+          result = helper.send(:ancestor)
+          expect(result).to match_array([:scope_1_o_1,:scope_2_o_1])
+          expect(result).not_to include(:scope_1_o_2, :scope_2_o_2)
+        end
+      end
   end
 end
